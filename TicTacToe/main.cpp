0bbc58916c2014a57ad5ab65eefae5c974f5f411
trent.wyatt <trent@trentwyatt.com>
2021-08-04 19:07:49 -0500

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by trent on 8/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
